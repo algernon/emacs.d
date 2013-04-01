d35230a1a8e78796f282985c5eb5cc57da22fb4f
Gergely Nagy <algernon@madhouse-project.org>
2013-04-01 22:43:22 +0200

    core/packs/: Add a sunrise-commander pack
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

---
+++ core/packs/pack-sunrise.el
@@ -0,0 +1,7 @@
+;; Last updated: <2013/04/01 21:59:16 algernon@madhouse-project.org>
+
+(add-to-list 'package-archives
+	     '("joseito-melpa" .
+	       "http://joseito.republika.pl/sunrise-commander/"))
+
+(packages-maybe-install '(sunrise-commander))
