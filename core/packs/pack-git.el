2411dda5a3a2f85c771d6f7f9ca824838f68056c
Gergely Nagy <algernon@madhouse-project.org>
2013-04-01 16:10:01 +0200

    core/packs/pack-git: Add magithub
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

--- core/packs/pack-git.el
+++ core/packs/pack-git.el
@@ -1,6 +1,6 @@
-;; Last updated: <2013/04/01 15:29:19 algernon@madhouse-project.org>
+;; Last updated: <2013/04/01 16:09:38 algernon@madhouse-project.org>
 
-(packages-maybe-install '(magit git-commit-mode))
+(packages-maybe-install '(magit git-commit-mode magithub))
 
 (global-set-key "\C-xg" 'magit-status)
 
