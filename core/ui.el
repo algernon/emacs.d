345489a6b894b81787594f6b214dc524d0ef7df7
Gergely Nagy <algernon@madhouse-project.org>
2013-04-01 22:44:10 +0200

    core/ui: Use a custom mode-line
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

--- core/ui.el
+++ core/ui.el
@@ -1,4 +1,4 @@
-;; Last updated: <2013/04/01 17:04:23 algernon@madhouse-project.org>
+;; Last updated: <2013/04/01 22:42:50 algernon@madhouse-project.org>
 
 (packages-maybe-install '(solarized-theme zenburn-theme color-theme))
 
@@ -40,3 +40,67 @@
 	(load-theme 'naquadah t)))
   (progn
     (load-theme 'solarized-light t)))
+
+;; Set the modeline
+(setq-default mode-line-format
+  (list
+
+   ;; the buffer name; the file name as a tool tip
+   mode-line-client
+   '(:eval (propertize "%b "
+		       'face 'font-lock-keyword-face
+		       'help-echo (buffer-file-name)))
+
+   ;; line and column
+   "(" ;; '%02' to set to 2 chars at least; prevents flickering
+   (propertize "%02l" 'face 'font-lock-type-face) ","
+   (propertize "%02c" 'face 'font-lock-type-face)
+   ") "
+
+   ;; relative position, size of file
+   "["
+   (propertize "%p" 'face 'font-lock-constant-face) ;; % above top
+   "/"
+   (propertize "%I" 'face 'font-lock-constant-face) ;; size
+   "] "
+
+   ;; the current major mode for the buffer.
+   "["
+   '(:eval (propertize "%m" 'face 'font-lock-string-face
+		       'help-echo buffer-file-coding-system))
+   "] "
+
+   ;; insert vs overwrite mode, input-method in a tooltip
+   "["
+   '(:eval (propertize (if overwrite-mode "Ovr" "Ins")
+		       'face 'font-lock-preprocessor-face
+		       'help-echo (concat "Buffer is in "
+					  (if overwrite-mode "overwrite" "insert") " mode")))
+
+   ;; was this buffer modified since the last save?
+   '(:eval (when (buffer-modified-p)
+	     (concat ","  (propertize "Mod"
+				      'face 'font-lock-warning-face
+				      'help-echo "Buffer has been modified"))))
+
+   ;; is this buffer read-only?
+   '(:eval (when buffer-read-only
+	     (concat ","  (propertize "RO"
+				      'face 'font-lock-type-face
+				      'help-echo "Buffer is read-only"))))
+   "] "
+
+   ;; add misc. mode line info
+   "-- " mode-line-misc-info "--"
+
+   ;; add minor modes
+   " ["
+   `(:propertize ("" minor-mode-alist)
+		 mouse-face mode-line-highlight
+		 face font-lock-keyword-face
+		 help-echo "mouse-1: minor-mode menu, mouse-2: minor-mode help, mouse-3: toggle minor modes"
+		 local-map ,mode-line-minor-mode-keymap)
+   " ]"
+
+   ;; fill the rest with spaces
+   mode-line-end-spaces))
