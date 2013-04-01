51bce9394ec0255f790a9632c3397e84c84c8795
Gergely Nagy <algernon@madhouse-project.org>
2013-04-01 15:30:16 +0200

    core/packs/pack-git: Enable commit sign-offs for magit
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

--- core/packs/pack-git.el
+++ core/packs/pack-git.el
@@ -1,5 +1,7 @@
-;; Last updated: <2013/04/01 13:46:33 algernon@madhouse-project.org>
+;; Last updated: <2013/04/01 15:29:19 algernon@madhouse-project.org>
 
 (packages-maybe-install '(magit git-commit-mode))
 
 (global-set-key "\C-xg" 'magit-status)
+
+(setq magit-commit-signoff t)
