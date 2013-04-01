90aba42a8b77d01128646ff262ac507f7c61d89a
Gergely Nagy <algernon@madhouse-project.org>
2013-04-01 17:22:46 +0200

    core/packs/pack-dired: Really enable omit mode
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

--- core/packs/pack-dired.el
+++ core/packs/pack-dired.el
@@ -1,10 +1,11 @@
-;; Last updated: <2013/04/01 12:37:14 algernon@madhouse-project.org>
+;; Last updated: <2013/04/01 17:11:53 algernon@madhouse-project.org>
 
 (require 'dired nil t)
 (require 'dired-x nil t)
 
-(setq dired-omit-files "^\\.?#\\|^\\.$\\|^\\.\\.$\\|^CVS$\\|,v$\\|^\\.arch-ids$\\|^{arch}\\|^,,\\|^\\.git$"
+(setq dired-omit-files "^\\.?#\\|^\\.$\\|^\\.\\.$\\|^CVS$\\|,v$\\|^\\.arch-ids$\\|^{arch}\\|^,,\\|^\\.git$\\|^\\.dirstamp$"
       dired-recursive-deletes 'top)
+(set-default 'dired-omit-mode t)
 
 (global-set-key "\C-x\C-d" 'ido-dired)
 
@@ -12,5 +13,4 @@
 (global-set-key "\C-xf" 'find-file)
 
 (add-hook 'dired-load-hook (function (lambda ()
-				       (dired-omit-mode)
 				       (load "dired-x"))))
