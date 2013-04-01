1381aa1f53ae64b0186cd06541b000ba8aaca9b9
Gergely Nagy <algernon@madhouse-project.org>
2013-04-01 18:46:34 +0200

    core/packs/pack-git: Commit log customisation
    Set auto-fill and flyspell-mode in magit-log-edit-mode.
    
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

--- core/packs/pack-git.el
+++ core/packs/pack-git.el
@@ -1,7 +1,17 @@
-;; Last updated: <2013/04/01 16:09:38 algernon@madhouse-project.org>
+;; Last updated: <2013/04/01 18:44:32 algernon@madhouse-project.org>
 
 (packages-maybe-install '(magit git-commit-mode magithub))
 
 (global-set-key "\C-xg" 'magit-status)
 
 (setq magit-commit-signoff t)
+
+(add-hook 'magit-log-edit-mode-hook
+	  (lambda ()
+	    (set-fill-column 72)
+	    (auto-fill-mode)))
+
+(eval-after-load "ispell"
+  '(when (executable-find ispell-program-name)
+     (add-hook 'magit-log-edit-mode-hook 'turn-on-flyspell)))
+
