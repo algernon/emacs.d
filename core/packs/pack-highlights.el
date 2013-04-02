3616cdb1a501e9a66c6f3112ef39b0003ba56d4a
Gergely Nagy <algernon@balabit.hu>
2013-04-02 12:33:40 +0200

    core/packs/pack-highlights: Enable nrepl-eval-sexp-fu
    Signed-off-by: Gergely Nagy <algernon@balabit.hu>

--- core/packs/pack-highlights.el
+++ core/packs/pack-highlights.el
@@ -1,5 +1,11 @@
-;; Last updated: <2013/04/01 18:54:00 algernon@madhouse-project.org>
+;; Last updated: <2013/04/02 12:32:37 algernon@madhouse-project.org>
 
-(packages-maybe-install '(volatile-highlights))
+(packages-maybe-install '(volatile-highlights highlight))
 (require 'volatile-highlights)
 (volatile-highlights-mode)
+
+(require 'highlight)
+(add-to-list 'load-path (concat user-emacs-directory
+                                "packages/nrepl-eval-sexp-fu"))
+(require 'nrepl-eval-sexp-fu)
+(setq nrepl-eval-sexp-fu-flash-duration 0.5)
