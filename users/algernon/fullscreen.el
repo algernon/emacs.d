3d86f6c7bb2dbedda70bb801c15347d2eaf46479
Gergely Nagy <algernon@madhouse-project.org>
2013-04-02 02:11:06 +0200

    users/algernon: Only maximise the frame under X
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

--- users/algernon/fullscreen.el
+++ users/algernon/fullscreen.el
@@ -1,4 +1,4 @@
-;; Last updated: <2013/04/02 02:07:59 algernon@madhouse-project.org>
+;; Last updated: <2013/04/02 02:10:32 algernon@madhouse-project.org>
 
 (defun fullscreen (&optional f)
        (interactive)
@@ -7,5 +7,7 @@
        (x-send-client-message nil 0 nil "_NET_WM_STATE" 32
                               '(2 "_NET_WM_STATE_MAXIMIZED_HORZ" 0)))
 
-(fullscreen)
+(when (eq window-system 'x)
+  (fullscreen))
+
 (global-set-key [f11] 'fullscreen)
