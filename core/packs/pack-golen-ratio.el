247429ff042d647e162fb958ae7cd39da9367eca
Gergely Nagy <algernon@madhouse-project.org>
2013-04-02 23:13:30 +0200

    core/packs/: Add a golden-ratio pack
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

---
+++ core/packs/pack-golen-ratio.el
@@ -0,0 +1,6 @@
+;; Last updated: <2013/04/02 23:13:07 algernon@madhouse-project.org>
+
+(add-to-list 'load-path (concat user-emacs-directory
+                                "packages/golden-ratio"))
+(require 'golden-ratio)
+(golden-ratio-enable)
