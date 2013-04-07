fb6920cf3160b6afcaf7d451f87c10d545bfe440
Gergely Nagy <algernon@madhouse-project.org>
2013-04-07 12:46:48 +0200

    core/behaviour: Do not show menu keybinds in the echo area
    The menu is turned off, so when launching something with M-x, do not
    show the menu binding in the echo area.
    
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

--- core/behaviour.el
+++ core/behaviour.el
@@ -1,5 +1,6 @@
-;; Last updated: <2013/04/01 12:19:29 algernon@madhouse-project.org>
+;; Last updated: <2013/04/07 12:43:49 algernon@madhouse-project.org>
 
-(setq sentence-end-double-space nil
+(setq easy-menu-precalculate-equivalent-keybindings nil
+      sentence-end-double-space nil
       whitespace-style '(face trailing lines-tail tabs)
       diff-switches "-u")
