c749e8e5dc702479ba40d00fec85ff76f6c92aa8
Gergely Nagy <algernon@madhouse-project.org>
2013-04-08 08:03:53 +0200

    diredful-conf: Remove "Hidden" too, for now
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

--- diredful-conf.el
+++ diredful-conf.el
@@ -1,4 +1,4 @@
 
-("Hidden" "Documentation" "Lisp source")
+("Documentation" "Lisp source")
 
-(("Hidden" (:foreground "dim gray") "\\\\." t nil nil nil) ("Documentation" (:foreground "spring green") "README.* NEWS.* LICENSE.* COPYING TODO.*" t nil nil nil) ("Lisp source" (:foreground "gold") "el clj cljs" nil nil nil nil))
+(("Documentation" (:foreground "spring green") "README.* NEWS.* LICENSE.* COPYING TODO.*" t nil nil nil) ("Lisp source" (:foreground "gold") "el clj cljs" nil nil nil nil))
