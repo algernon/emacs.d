9e558f6c057a68cad274c2d5de6187ef235ed930
Gergely Nagy <algernon@madhouse-project.org>
2013-04-08 07:59:52 +0200

    diredful-conf.el: Configuration for diredful
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

---
+++ diredful-conf.el
@@ -0,0 +1,4 @@
+
+("Lisp source" "Hidden files" "Documentation")
+
+(("Lisp source" (:foreground "gold") "el clj cljs" nil nil nil nil) ("Hidden files" (:foreground "dim gray") "^." t nil nil nil) ("Documentation" (:foreground "spring green") "README.* NEWS.* LICENSE.* COPYING" t nil nil nil))
