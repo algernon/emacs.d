bac5f632636461c0c8bd941a6bd3cf07ab95111f
Gergely Nagy <algernon@balabit.hu>
2013-04-09 16:48:32 +0200

    core/misc: Collection of misc. functions
    Signed-off-by: Gergely Nagy <algernon@balabit.hu>

---
+++ core/misc.el
@@ -0,0 +1,8 @@
+;; Last updated: <2013/04/09 16:48:13 algernon@madhouse-project.org>
+
+(defun what-face (pos)
+  (interactive "d")
+  (let ((face (or (get-char-property (point) 'read-face-name)
+                  (get-char-property (point) 'face))))
+    (if face (message "Face: %s" face) (message "No face at %d"
+                                                pos))))
