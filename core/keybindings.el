c71d71b30e1082c3ac75d85acdc93a4464cb1cda
Gergely Nagy <algernon@madhouse-project.org>
2013-06-10 20:58:05 +0200

    core/keybindings: Borrow C-a behaviour from prelude
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

--- core/keybindings.el
+++ core/keybindings.el
@@ -1,4 +1,4 @@
-;; Last updated: <2013/05/30 12:57:05 algernon@madhouse-project.org>
+;; Last updated: <2013/06/10 20:57:40 algernon@madhouse-project.org>
 
 ;; Misc. bindigs
 (defun goto-line-with-feedback ()
@@ -83,3 +83,29 @@
   (delete-indentation 1))
 
 (global-set-key (kbd "M-^") 'top-join-line)
+
+(defun prelude-move-beginning-of-line (arg)
+  "Move point back to indentation of beginning of line.
+
+Move point to the first non-whitespace character on this line.
+If point is already there, move to the beginning of the line.
+Effectively toggle between the first non-whitespace character and
+the beginning of the line.
+
+If ARG is not nil or 1, move forward ARG - 1 lines first.  If
+point reaches the beginning or end of the buffer, stop there."
+  (interactive "^p")
+  (setq arg (or arg 1))
+
+  ;; Move lines first
+  (when (/= arg 1)
+    (let ((line-move-visual nil))
+      (forward-line (1- arg))))
+
+  (let ((orig-point (point)))
+    (back-to-indentation)
+    (when (= orig-point (point))
+      (move-beginning-of-line 1))))
+
+(global-set-key [remap move-beginning-of-line]
+                'prelude-move-beginning-of-line)
