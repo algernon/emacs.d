a51f53e262c41e80bdd37bf67af28fe8416e60a9
Gergely Nagy <algernon@balabit.hu>
2013-11-04 16:03:41 +0100

    core/misc.el: New function: insert-rfc-date
    Signed-off-by: Gergely Nagy <algernon@balabit.hu>

--- core/misc.el
+++ core/misc.el
@@ -1,4 +1,4 @@
-;; Last updated: <2013/04/09 16:48:13 algernon@madhouse-project.org>
+;; Last updated: <2013/11/04 08:55:35 algernon@madhouse-project.org>
 
 (defun what-face (pos)
   (interactive "d")
@@ -10,3 +10,9 @@
 (setq safe-local-variable-values '((encoding . utf-8)
                                    (ispell-dictionary . "magyar")
                                    (ispell-dictionary . "british")))
+
+(defun insert-rfc-date ()
+  "Inserts RFC2822 formatted date at point"
+  (interactive)
+
+  (insert (format-time-string "%a, %e %b %Y %H:%M:%S %z")))
