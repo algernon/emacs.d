5fde177af069ba07badcc77849b745e94ba98f56
Gergely Nagy <algernon@balabit.hu>
2014-01-29 12:09:26 +0100

    core/packs/pack-guide-key.el: More configuration
    Move the popup window to the bottom, and enable
    recursive-key-sequence-flag.
    
    Signed-off-by: Gergely Nagy <algernon@balabit.hu>

--- core/packs/pack-guide-key.el
+++ core/packs/pack-guide-key.el
@@ -1,6 +1,8 @@
-;; Last updated: <2014/01/29 11:55:03 algernon@madhouse-project.org>
+;; Last updated: <2014/01/29 12:09:05 algernon@madhouse-project.org>
 
 (packages-maybe-install '(guide-key))
 
 (setq guide-key/guide-key-sequence '("C-x" "C-c" "C-o"))
+(setq guide-key/recursive-key-sequence-flag t)
+(setq guide-key/popup-window-position 'bottom)
 (guide-key-mode 1)
