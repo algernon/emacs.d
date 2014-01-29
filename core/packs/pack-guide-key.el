09d8040ad7a79fb8f1e2e7c8f46f30e842d1f8f4
Gergely Nagy <algernon@balabit.hu>
2014-01-29 11:55:20 +0100

    core/packs/pack-guide-key.el: New file
    Lets experiment with guide-key.
    
    Signed-off-by: Gergely Nagy <algernon@balabit.hu>

---
+++ core/packs/pack-guide-key.el
@@ -0,0 +1,6 @@
+;; Last updated: <2014/01/29 11:55:03 algernon@madhouse-project.org>
+
+(packages-maybe-install '(guide-key))
+
+(setq guide-key/guide-key-sequence '("C-x" "C-c" "C-o"))
+(guide-key-mode 1)
