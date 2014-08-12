03e11e91f87bbf8d0900a7c09ec4e571896d7f0e
Gergely Nagy <algernon@madhouse-project.org>
2014-08-12 14:27:35 +0200

    packs/auto-complete: Switch to company-mode
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

--- core/packs/pack-auto-complete.el
+++ core/packs/pack-auto-complete.el
@@ -1,11 +1,5 @@
-;; Last updated: <2013/04/03 10:24:02 algernon@madhouse-project.org>
+;; Last updated: <2014/08/12 14:27:20 algernon@madhouse-project.org>
 
-(packages-maybe-install '(fuzzy auto-complete))
+(packages-maybe-install '(fuzzy company))
 
-(require 'auto-complete-config)
-(ac-config-default)
-
-(global-auto-complete-mode t)
-(diminish 'auto-complete-mode)
-
-(global-set-key (kbd "<backtab>") 'auto-complete)
+(global-company-mode)
