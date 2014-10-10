146742dea38e5514da636ef7d653fbc021cb8690
Gergely Nagy <algernon@madhouse-project.org>
2014-10-10 18:27:39 +0200

    users/algernon/fullscreen.el: Allow maximizing any frame
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

--- users/algernon/fullscreen.el
+++ users/algernon/fullscreen.el
@@ -1,8 +1,8 @@
-;; Last updated: <2014/10/02 07:58:08 algernon@madhouse-project.org>
+;; Last updated: <2014/10/08 10:04:46 algernon@madhouse-project.org>
 
 (defun fullscreen (&optional f)
   (interactive)
-  (shell-command (concat "wmctrl -i -r " (frame-parameter nil 'outer-window-id) " -btoggle,maximized_vert,maximized_horz")))
+  (shell-command (concat "wmctrl -i -r " (frame-parameter f 'outer-window-id) " -btoggle,maximized_vert,maximized_horz")))
 
 (when (and (eq window-system 'x)
            (not (is-presentingp)))
