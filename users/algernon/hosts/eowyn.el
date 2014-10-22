845330da463cbdbe5c46ff715445263de81415de
Gergely Nagy <algernon@madhouse-project.org>
2014-10-22 16:07:37 +0200

    users/algernon[@eowyn]: Config update
    Always set up the theme, Emacs is launched by systemd with emacs
    --daemon. Also add supercite config, because that looks cool. This
    latter one fixes #2.
    
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

--- users/algernon/hosts/eowyn.el
+++ users/algernon/hosts/eowyn.el
@@ -3,11 +3,13 @@
 ;; Copyright (C) 2014
 ;; Gergely Nagy <algernon@madhouse-project.org>
 
-;; Last updated: <2014/10/02 07:38:58 algernon@madhouse-project.org>
-
-(require 'gnus)
+;; Last updated: <2014/10/22 16:05:28 algernon@madhouse-project.org>
 
 (packages-maybe-install '(molokai-theme cyberpunk-theme))
+
+(add-to-list 'load-path
+             (expand-file-name "~/.local/share/emacs/site-lisp/gnus"))
+(require 'gnus)
 
 (setq gnus-select-method '(nnimap "algernon@madhouse-project.org"
                                   (nnimap-address "imap.madhouse-project.org")
@@ -37,9 +39,28 @@
          (address "algernon@madhouse-project.org"))
         ))
 
-(when (and window-system
-           (>= emacs-major-version 24))
-  (if (is-presentingp)
-      (load-theme 'cyberpunk)
-    (load-theme 'molokai))
-  (sml/setup))
+;; Supercite setup
+(autoload 'sc-cite-original "supercite")
+(autoload 'sc-submit-bug-report "supercite")
+(add-hook 'mail-citation-hook 'sc-cite-original)
+
+(setq message-cite-function 'sc-cite-original)
+
+(setq news-reply-header-hook nil)
+(setq sc-auto-fill-region-p nil)
+(setq sc-fixup-whitespace-p t)
+(setq sc-citation-leader "    ")
+(setq sc-citation-delimiter ">")
+(setq sc-citation-separator " ")
+(setq sc-preferred-attribution-list
+      '("sc-lastchoice" "x-attribution" "firstname" "initials" "lastname"))
+
+;; Frame setup
+(add-hook 'after-make-frame-functions 'fullscreen)
+(add-hook 'after-make-frame-functions (lambda (f)
+                                        (select-frame-set-input-focus f)))
+
+(if (is-presentingp)
+    (load-theme 'cyberpunk)
+  (load-theme 'molokai))
+(sml/setup)
