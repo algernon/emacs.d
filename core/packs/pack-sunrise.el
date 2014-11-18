0a800646ae8d252475a88893bf5af5b5cd6a063c
Gergely Nagy <algernon@madhouse-project.org>
2014-11-18 15:51:14 +0100

    packs: drop pack-sunrise.el
    I'm not using sunrise commander anymore, drop it.
    
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

--- core/packs/pack-sunrise.el
+++
@@ -1,7 +0,0 @@
-;; Last updated: <2013/04/02 00:05:25 algernon@madhouse-project.org>
-
-(add-to-list 'package-archives
-             '("joseito-melpa" .
-               "http://joseito.republika.pl/sunrise-commander/"))
-
-(packages-maybe-install '(sunrise-commander))
