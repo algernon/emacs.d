9589f1ec941e23eccd58bcc3b2bff251c68dcc98
Gergely Nagy <algernon@madhouse-project.org>
2015-01-06 15:57:37 +0100

    core/packs/pack-timestamp: Fix the setup
    Don't use use-package for time-stamp.
    
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

--- core/packs/pack-timestamp.el
+++ core/packs/pack-timestamp.el
@@ -1,10 +1,9 @@
-;; Last updated: <2015/01/06 13:31:14 algernon@madhouse-project.org>
+;; Last updated: <2015/01/06 15:57:24 algernon@madhouse-project.org>
 
-(use-package time-stamp
-  :init (add-hook 'write-file-hooks '(lambda ()
-                                       (time-stamp)))
-  :config (setq time-stamp-active t
-                time-stamp-start "[lL]ast [uU]pdated:[ 	]+\\\\?[\"<]+"
-                time-stamp-line-limit 20
-                time-stamp-format (concat "%:y/%02m/%02d %02H:%02M:%02S "
-                                          user-mail-address)))
+(add-hook 'write-file-hooks '(lambda ()
+                               (time-stamp)))
+(setq time-stamp-active t
+      time-stamp-start "[lL]ast [uU]pdated:[ 	]+\\\\?[\"<]+"
+      time-stamp-line-limit 20
+      time-stamp-format (concat "%:y/%02m/%02d %02H:%02M:%02S "
+                                user-mail-address))
