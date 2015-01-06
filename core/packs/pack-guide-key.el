e7ccfd731082f3d162e197bfbc6ad1d6d3ae2149
Gergely Nagy <algernon@madhouse-project.org>
2015-01-06 14:01:08 +0100

    core/packs/pack-guide-key: Diminish guide-key-mode
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

--- core/packs/pack-guide-key.el
+++ core/packs/pack-guide-key.el
@@ -1,6 +1,7 @@
-;; Last updated: <2015/01/06 12:57:56 algernon@madhouse-project.org>
+;; Last updated: <2015/01/06 14:01:00 algernon@madhouse-project.org>
 
 (use-package guide-key
+  :diminish guide-key-mode
   :init (guide-key-mode 1)
   :config (setq
            guide-key/guide-key-sequence '("C-x" "C-c" "C-o")
