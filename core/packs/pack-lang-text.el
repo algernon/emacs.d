90c1882d2c719b3fef166593bcf36ef4bcb6ebdc
Gergely Nagy <algernon@madhouse-project.org>
2015-01-07 14:26:51 +0100

    core/packs/pack-lang-text.el: Fix indentation
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

--- core/packs/pack-lang-text.el
+++ core/packs/pack-lang-text.el
@@ -1,4 +1,4 @@
-;; Last updated: <2015/01/06 13:24:38 algernon@madhouse-project.org>
+;; Last updated: <2015/01/07 14:26:42 algernon@madhouse-project.org>
 
 (add-to-list 'auto-mode-alist
                      '("README$\\|NEWS$\\|INSTALL$\\|AUTHORS$\\|THANKS$"
@@ -6,9 +6,9 @@
 
 (setq flyspell-default-dictionary "british")
 
-            (use-package ispell
-              :init (when (executable-find ispell-program-name)
-                      (add-hook 'text-mode-hook 'turn-on-flyspell)))
+(use-package ispell
+  :init (when (executable-find ispell-program-name)
+          (add-hook 'text-mode-hook 'turn-on-flyspell)))
 
-            (add-hook 'text-mode-hook '(lambda ()
-                                         (turn-on-auto-fill)))
+(add-hook 'text-mode-hook '(lambda ()
+                             (turn-on-auto-fill)))
