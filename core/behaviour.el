ae63b7e372ffeabaf4d0be393aea0f24b128847e
Gergely Nagy <algernon@madhouse-project.org>
2015-01-07 10:50:55 +0100

    core/behaviour.el: Show trailing spaces by default
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

--- core/behaviour.el
+++ core/behaviour.el
@@ -1,7 +1,8 @@
-;; Last updated: <2013/06/01 09:55:32 algernon@madhouse-project.org>
+;; Last updated: <2015/01/07 10:49:08 algernon@madhouse-project.org>
 
 (setq easy-menu-precalculate-equivalent-keybindings nil
       sentence-end-double-space nil
       whitespace-style '(face trailing lines-tail tabs)
       whitespace-line-column 80
-      diff-switches "-u")
+      diff-switches "-u"
+      show-trailing-whitespace 1)
