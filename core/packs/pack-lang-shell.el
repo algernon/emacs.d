b3ac801fa4cfd2b73e7b0c680bd668de62ee4ac2
Gergely Nagy <algernon@madhouse-project.org>
2015-01-07 15:11:06 +0100

    core/packs/pack-lang-shell.el: Fix the shell mode stuff
    Don't use use-package for these.
    
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

--- core/packs/pack-lang-shell.el
+++ core/packs/pack-lang-shell.el
@@ -1,8 +1,6 @@
-;; Last updated: <2015/01/06 13:23:32 algernon@madhouse-project.org>
+;; Last updated: <2015/01/07 15:10:34 algernon@madhouse-project.org>
 
-(use-package shell-mode
-  :config (progn
-            (setq sh-basic-offset 8
-                  sh-indentation 8)
+(setq-default sh-basic-offset 8
+              sh-indentation 8)
 
-            (add-hook 'shell-mode-hook 'pcomplete-shell-setup)))
+(add-hook 'shell-mode-hook 'pcomplete-shell-setup)
