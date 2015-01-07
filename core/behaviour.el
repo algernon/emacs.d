a36073c9ac08671ca2621e832d7f2688d739a280
Gergely Nagy <algernon@madhouse-project.org>
2015-01-07 10:52:45 +0100

    core/behaviour.el: Drop easy-menu stuff
    easy-menu-precalculate-equivalent-keybindings is obsolete since 23.1,
    and is nil by default. Drop it, to silence a warning, and clean up the
    config.
    
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

--- core/behaviour.el
+++ core/behaviour.el
@@ -1,7 +1,6 @@
-;; Last updated: <2015/01/07 10:49:08 algernon@madhouse-project.org>
+;; Last updated: <2015/01/07 10:52:39 algernon@madhouse-project.org>
 
-(setq easy-menu-precalculate-equivalent-keybindings nil
-      sentence-end-double-space nil
+(setq sentence-end-double-space nil
       whitespace-style '(face trailing lines-tail tabs)
       whitespace-line-column 80
       diff-switches "-u"
