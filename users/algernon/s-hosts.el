f7b672219b6900b6346d58547d61e47201b2548b
Gergely Nagy <algernon@madhouse-project.org>
2015-01-08 11:21:31 +0100

    users/algernon/s-hosts.el: Use load-file-sans-extension
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

--- users/algernon/s-hosts.el
+++ users/algernon/s-hosts.el
@@ -1,5 +1,5 @@
-;; Last updated: <2013/04/01 17:33:28 algernon@madhouse-project.org>
+;; Last updated: <2015/01/08 11:16:00 algernon@madhouse-project.org>
 
 (let ((user-host-file (concat aec-user-dir "/hosts/" system-name ".el")))
   (when (file-exists-p user-host-file)
-    (load user-host-file)))
+    (load-file-sans-extension user-host-file)))
