a4d01d2c9a29ace8099da044e4143590145864c2
Gergely Nagy <algernon@madhouse-project.org>
2015-01-08 13:21:47 +0100

    packs/lang/autoconf: Fix ,, keychord
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

--- lisp/aec/packs/key-chord.el
+++ lisp/aec/packs/key-chord.el
@@ -1,10 +1,10 @@
-;; Last updated: <2015/01/08 12:46:54 algernon@madhouse-project.org>
+;; Last updated: <2015/01/08 13:21:37 algernon@madhouse-project.org>
 
 (use-package key-chord
   :ensure t
   :init (key-chord-mode 1)
   :config (progn
-            (key-chord-define-global ",," #'indent-for-comment-foo)
+            (key-chord-define-global ",," #'indent-for-comment)
             (key-chord-define-global "''" "`'\C-b")))
 
 (provide 'aec/packs/key-chord)
