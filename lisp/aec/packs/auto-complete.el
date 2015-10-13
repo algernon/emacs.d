903b33f3bacc0bf50d5fb580c29482b30d45a812
Gergely Nagy <algernon@madhouse-project.org>
2015-10-13 14:09:03 +0200

    aec/packs/auto-complete: Add company-emoji, because.
    ⛄🐙😄
    
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

--- lisp/aec/packs/auto-complete.el
+++ lisp/aec/packs/auto-complete.el
@@ -1,10 +1,22 @@
-;; Last updated: <2015/06/26 12:16:58 algernon@madhouse-project.org>
+;; Last updated: <2015/10/07 15:52:24 algernon@madhouse-project.org>
 
 (use-package company
   :ensure t
   :defer t
   :diminish company-mode
-  :config (global-company-mode))
+  :init (progn
+          (global-company-mode)
+          (use-package company-emoji
+            :ensure t
+            :defer t
+            :init (progn
+                    (add-to-list 'company-backends 'company-emoji)
+                    (set-fontset-font "fontset-default"
+                                      '(#x1f300 . #x1f5ff) "Symbola")
+                    (set-fontset-font "fontset-default"
+                                      '(#x1f600 . #x1f64f) "Symbola")
+                    (set-fontset-font "fontset-default"
+                                      '(#x2600  . #x27bf ) "Symbola")))))
 
 (use-package fuzzy
   :ensure t
