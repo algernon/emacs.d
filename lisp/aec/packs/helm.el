86e557c655d90ac91267093f5959e965ffc1b96d
Gergely Nagy <algernon@cloudera.com>
2015-10-28 09:43:40 +0100

    aec/packs/helm: Use -flx and -fuzzer
    Also use helm-M-x for M-x.
    
    Signed-off-by: Gergely Nagy <algernon@cloudera.com>

--- lisp/aec/packs/helm.el
+++ lisp/aec/packs/helm.el
@@ -1,10 +1,15 @@
-;; Last updated: <2015/01/08 12:45:52 algernon@madhouse-project.org>
+;; Last updated: <2015/10/28 08:37:27 algernon@madhouse-project.org>
 
 (use-package helm
   :ensure t
   :init (require 'helm-config)
-  :bind (("C-c h" . helm-command-prefix))
+  :bind (("C-c h" . helm-command-prefix)
+         ("M-x" . helm-M-x))
   :config (progn
+            (use-package helm-flx
+              :ensure t)
+            (use-package helm-fuzzier
+              :ensure t)
             (when (executable-find "curl")
               (setq helm-google-suggest-use-curl-p t))
 
@@ -12,7 +17,9 @@
                   helm-buffers-fuzzy-matching           t
                   helm-move-to-line-cycle-in-source     t
                   helm-ff-search-library-in-sexp        t
-                  helm-ff-file-name-history-use-recentf t)
+                  helm-ff-file-name-history-use-recentf t
+                  helm-M-x-fuzzy-match                  t
+                  helm-mode-fuzzy-match                 t)
 
             (define-key helm-command-map (kbd "o")     #'helm-occur)
             (define-key helm-command-map (kbd "g")     #'helm-do-grep)
