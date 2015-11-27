167ff2ec56d7ef58a2d074e64f57816a2082d775
Gergely Nagy <algernon@madhouse-project.org>
2015-11-27 14:07:46 +0100

    +apps/feed-reader: Use a custom header line
    Again, this requires my custom fork of elfeed to work. Otherwise these
    do nothing.
    
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

--- layers/+apps/feed-reader/packages.el
+++ layers/+apps/feed-reader/packages.el
@@ -5,6 +5,71 @@
 ;;
 ;; [1]: https://github.com/algernon/elfeed
 
+(defun feed-reader/stats ()
+  "Count the number of entries and feeds being currently displayed."
+  (if (and elfeed-search-filter-active elfeed-search-filter-overflowing)
+      (list 0 0 0)
+    (cl-loop with feeds = (make-hash-table :test 'equal)
+             for entry in elfeed-search-entries
+             for feed = (elfeed-entry-feed entry)
+             for url = (elfeed-feed-url feed)
+             count entry into entry-count
+             count (elfeed-tagged-p 'unread entry) into unread-count
+             do (puthash url t feeds)
+             finally
+             (cl-return
+              (list unread-count entry-count (hash-table-count feeds))))))
+
+(defun feed-reader/search-header ()
+  "Returns the string to be used as the Elfeed header."
+  (let* ((separator-left (intern (format "powerline-%s-%s"
+                                         (powerline-current-separator)
+                                         (car powerline-default-separator-dir))))
+         (separator-right (intern (format "powerline-%s-%s"
+                                          (powerline-current-separator)
+                                          (cdr powerline-default-separator-dir)))))
+    (if (zerop (elfeed-db-last-update))
+        (elfeed-search--intro-header)
+      (let* ((db-time (seconds-to-time (elfeed-db-last-update)))
+             (update (format-time-string "%Y-%m-%d %H:%M:%S %z" db-time))
+             (stats (feed-reader/stats))
+             (search-filter (cond
+                             (elfeed-search-filter-active
+                              "")
+                             (elfeed-search-filter
+                              elfeed-search-filter)
+                             ("")))
+             (lhs (list
+                   (powerline-raw (concat search-filter " ") 'powerline-active1 'l)
+                   (funcall separator-right 'powerline-active1 'mode-line)))
+             (center (list
+                      (funcall separator-left 'mode-line 'powerline-active2)
+                      (destructuring-bind (unread entry-count feed-count) stats
+                        (let* ((content (format " %d/%d:%d" unread entry-count feed-count))
+                               (help-text nil)
+                               )
+                          (if url-queue
+                              (let* ((total (length url-queue))
+                                     (in-process (cl-count-if #'url-queue-buffer url-queue)))
+                                (setf content (concat content " (*)"))
+                                (setf help-text (format " %d feeds pending, %d in process ... "
+                                                        total in-process))))
+                          (propertize content
+                                      'face 'powerline-active2
+                                      'help-echo help-text)))
+                      (funcall separator-right 'powerline-active2 'mode-line)
+                      )
+                     )
+             (rhs (list
+                   (funcall separator-left 'mode-line 'powerline-active1)
+                   (powerline-raw (concat " " update) 'powerline-active1 'r))))
+
+        (concat (powerline-render lhs)
+                (powerline-fill-center nil (/ (powerline-width center) 2.0))
+                (powerline-render center)
+                (powerline-fill nil (powerline-width rhs))
+                (powerline-render rhs))))))
+
 (defun feed-reader/init-elfeed ()
   (use-package elfeed
     :commands elfeed
@@ -57,4 +122,6 @@
       (setq-default elfeed-max-connections 2
                     elfeed-search-filter "+unread")
 
-      (setf url-queue-timeout 30))))
+      (setf url-queue-timeout 30)
+
+      (setq elfeed-search-header-function #'feed-reader/search-header))))
