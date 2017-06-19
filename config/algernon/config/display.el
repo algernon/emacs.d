7ff9eba62261b90ebb6c94591cb3ced761743db0
Gergely Nagy <algernon@madhouse-project.org>
2017-06-19 14:42:17 +0200

    display: Set writeroom-width to 0.75
    80 chars is too narrow for some of the code I'm working on, bump it up to 75% of
    the screen instead.
    
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

--- config/algernon/config/display.el
+++ config/algernon/config/display.el
@@ -1,5 +1,5 @@
 ;;;; ~/.emacs.d/ -- algernon's Emacs configuration     -*- no-byte-compile: t -*-
-;; Last updated: <2017/06/18 11:53:56 algernon@madhouse-project.org>
+;; Last updated: <2017/06/19 14:41:57 algernon@madhouse-project.org>
 ;;
 ;; Copyright (C) 2000, 2001, 2002, 2003, 2004, 2005, 2010, 2011,
 ;;               2012, 2013, 2014, 2015, 2016, 2017
@@ -31,7 +31,8 @@
 
 (defun algernon/config/display ()
   (setq popwin:close-popup-window-timer-interval 0.1
-        scroll-margin 3)
+        scroll-margin 3
+        writeroom-width 0.75)
 
   (add-hook 'after-make-frame-functions
             (lambda (buffer)
