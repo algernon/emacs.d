f0bf15a9dc5ccde6813cee3666ac04e3e25d779f
Gergely Nagy <algernon@madhouse-project.org>
2017-07-21 16:04:24 +0200

    layers/distraction-free: Switch from writeroom to Olivetti
    Fixes #13.
    
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

--- layers/+look-n-feel/distraction-free/packages.el
+++ layers/+look-n-feel/distraction-free/packages.el
@@ -1,6 +1,6 @@
-;;; packages.el --- writeroom Layer packages File for Spacemacs
+;;; packages.el --- distraction-free packages File for Spacemacs
 ;;
-;; Copyright (c) 2015 Gergely Nagy
+;; Copyright (c) 2015, 2017 Gergely Nagy
 ;;
 ;; Author: Gergely Nagy <algernon@madhouse-project.org>
 ;; URL: https://github.com/algernon/spacemacs-config
@@ -11,17 +11,26 @@
 
 (setq distraction-free-packages
       '(
-        writeroom-mode
+        olivetti
         ))
 
-(defun distraction-free/init-writeroom-mode ()
-  "Initialize writeroom-mode"
+(defun distraction-free/init-olivetti ()
+  (defun olivetti ()
+    "Integrate `olivetti-mode' and `spacemacs/toggle-maximize-buffer'."
+    (interactive)
+    (if olivetti-mode
+        (spacemacs/toggle-maximize-buffer)
+      (spacemacs/toggle-maximize-buffer)
+      (olivetti-mode 1)))
 
-  (use-package writeroom-mode
-    :defer t
+  (use-package olivetti
     :init (progn
-            (defun spacemacs/writeroom-mode ()
-              (interactive)
-
-              (evil-leader/set-key "tW" #'writeroom-mode))
-            (evil-leader/set-key "tW" #'spacemacs/writeroom-mode))))
+            (spacemacs/set-leader-keys
+              "wo" 'olivetti
+              "wOs" 'olivetti-shrink
+              "wOe" 'olivetti-expand
+              "wOm" 'olivetti-toggle-hide-mode-line)
+            (setq olivetti-body-width 0.75
+                  olivetti-hide-mode-line t))
+    :config (advice-add 'spacemacs/toggle-maximize-buffer
+                        :after (lambda (&rest args) (olivetti-mode 0)))))
