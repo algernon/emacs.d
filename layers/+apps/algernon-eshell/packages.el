b8f5f7643ca3f1d2a9b61798f152c0d78258d0c1
Gergely Nagy <algernon@madhouse-project.org>
2017-07-24 17:20:16 +0200

    layers/algernon-eshell: Drop an unused let binding
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

--- layers/+apps/algernon-eshell/packages.el
+++ layers/+apps/algernon-eshell/packages.el
@@ -1,5 +1,5 @@
 ;;;; ~/.emacs.d/ -- algernon's Emacs configuration     -*- no-byte-compile: t -*-
-;; Last updated: <2017/07/24 14:15:03 algernon@madhouse-project.org>
+;; Last updated: <2017/07/24 17:19:49 algernon@madhouse-project.org>
 ;;
 ;; Copyright (C) 2000, 2001, 2002, 2003, 2004, 2005, 2010, 2011,
 ;;               2012, 2013, 2014, 2015, 2016, 2017
@@ -82,8 +82,7 @@
 
                 (let* ((command (format "git grep -n %s" (s-join " " args)))
                        (buffer (generate-new-buffer "*git-grep*"))
-                       (buffer-name (format "#<buffer %s>" buffer))
-                       (eshell-buffer (current-buffer)))
+                       (buffer-name (format "#<buffer %s>" buffer)))
                   (eshell-do-eval
                    (eshell-parse-command (format "format '-*- mode: grep; default-directory: \"%s\" -*-\n\n%s\n' >%s"
                                                  (eshell/pwd) command buffer-name)))
