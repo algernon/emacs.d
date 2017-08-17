6c94966ef62cf1094b27048ad068c4db0282282c
Gergely Nagy <emacs@gergo.csillger.hu>
2017-08-17 11:30:23 +0200

    layers/algernon-eshell: Force write history in `eshell/x`
    For some reason, my history was rarely saved if at all. Force writing it when
    calling `eshell/x` to make the situation a bit better.
    
    Signed-off-by: Gergely Nagy <emacs@gergo.csillger.hu>

--- layers/+apps/algernon-eshell/packages.el
+++ layers/+apps/algernon-eshell/packages.el
@@ -1,5 +1,5 @@
 ;;;; ~/.emacs.d/ -- algernon's Emacs configuration     -*- no-byte-compile: t -*-
-;; Last updated: <2017/08/08 13:53:32 algernon@madhouse-project.org>
+;; Last updated: <2017/08/17 00:20:00 algernon@madhouse-project.org>
 ;;
 ;; Copyright (C) 2000, 2001, 2002, 2003, 2004, 2005, 2010, 2011,
 ;;               2012, 2013, 2014, 2015, 2016, 2017
@@ -51,6 +51,7 @@
               ;; Aliases & commands
 
               (defun eshell/x ()
+                (eshell-write-history)
                 (eshell/exit))
 
               (defun eshell/up (dir)
