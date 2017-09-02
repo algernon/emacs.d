bf2fbac02d585946a6b897f1cfd0661510bbdbeb
Gergely Nagy <emacs@gergo.csillger.hu>
2017-09-02 07:21:02 +0200

    layers/+elfeed: Add some mail-like bindings to elfeed-search
    Fixes #19.
    
    Signed-off-by: Gergely Nagy <emacs@gergo.csillger.hu>

--- layers/+apps/algernon-elfeed/packages.el
+++ layers/+apps/algernon-elfeed/packages.el
@@ -1,5 +1,5 @@
 ;;;; ~/.emacs.d/ -- algernon's Emacs configuration     -*- no-byte-compile: t -*-
-;; Last updated: <2017/08/18 16:23:24 algernon@madhouse-project.org>
+;; Last updated: <2017/09/02 07:20:59 algernon@madhouse-project.org>
 ;;
 ;; Copyright (C) 2000, 2001, 2002, 2003, 2004, 2005, 2010, 2011,
 ;;               2012, 2013, 2014, 2015, 2016, 2017
@@ -86,6 +86,13 @@
                      "q" 'quit-window
                      "n" 'elfeed-show-next
                      "p" 'elfeed-show-prev)
-                   ))
+
+                   (evilified-state-evilify-map elfeed-search-mode-map
+                     :mode elfeed-search-mode
+                     :eval-after-load elfeed-search
+                     :bindings
+                     (kbd "d") 'elfeed-search-untag-all-unread
+                     (kbd "u") 'elfeed-search-tag-all-unread
+                     (kbd "=") 'elfeed-search-update)))
 
   (setf url-queue-timeout 30))
