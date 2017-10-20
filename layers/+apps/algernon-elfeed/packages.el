81174b00b4ee3b22cfa8557c9755e0efb9a0a43a
Gergely Nagy <emacs@gergo.csillger.hu>
2017-10-20 10:41:52 +0200

    layers/+algernon-elfeed: Update the default elfeed-search-filter
    Signed-off-by: Gergely Nagy <emacs@gergo.csillger.hu>

--- layers/+apps/algernon-elfeed/packages.el
+++ layers/+apps/algernon-elfeed/packages.el
@@ -1,5 +1,5 @@
 ;;;; ~/.emacs.d/ -- algernon's Emacs configuration     -*- no-byte-compile: t -*-
-;; Last updated: <2017/09/02 16:55:30 algernon@madhouse-project.org>
+;; Last updated: <2017/10/20 10:41:39 algernon@madhouse-project.org>
 ;;
 ;; Copyright (C) 2000, 2001, 2002, 2003, 2004, 2005, 2010, 2011,
 ;;               2012, 2013, 2014, 2015, 2016, 2017
@@ -71,7 +71,7 @@
                                        (elfeed-update))))
 
                    (setq elfeed-max-connections 2
-                         elfeed-search-filter "+unread -9gag -comic"
+                         elfeed-search-filter "+unread"
                          elfeed-goodies/wide-threshold 0.25
                          elfeed-goodies/entry-pane-position 'top
                          elfeed-goodies/entry-pane-size 0.85
